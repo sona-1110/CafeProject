293b871173768cbd969df62845ceb1b37338ec53
gsrushti27 <srushtighadage16@gmail.com>
2024-12-19 11:43:32 +0530

    first commit

---
+++ Frontend/caffefront/src/app/manage-services/manage-services.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-services',
+  imports: [],
+  templateUrl: './manage-services.component.html',
+  styleUrl: './manage-services.component.css'
+})
+export class ManageServicesComponent {
+
+}
---
+++ Frontend/caffefront/src/app/view-register/view-register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-register',
+  imports: [],
+  templateUrl: './view-register.component.html',
+  styleUrl: './view-register.component.css'
+})
+export class ViewRegisterComponent {
+
+}
